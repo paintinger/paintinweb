88735d4881572616d5904a322267306e37aafec5
Joel Paintinger <paintinger1@hotmail.com>
2025-07-19 03:21:22 -0300

    Create send_email.php

---
+++ send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+
+    // Configuração do email
+    $to = "seu-email@dominio.com"; // Altere para o seu e-mail
+    $subject = "Mensagem de Contato - PaintinWeb";
+    $body = "Nome: $nome\nEmail: $email\nMensagem:\n$mensagem";
+    $headers = "From: $email";
+
+    // Envio do e-mail
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Ocorreu um erro ao enviar a mensagem.";
+    }
+}
+?>
